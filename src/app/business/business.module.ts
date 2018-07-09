56b775f1c792eca56e1f64fc6d2f4223acbab5e9
David Jose Salazar Moreno <dsalazmo@usersad.everis.int>
2018-07-09 17:42:44 -0500

    refactor(business.module): Add an apiService.

---
+++ src/app/business/business.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+
+import { ApiService } from './services/api.services';
+
+@NgModule({
+  imports: [],
+  exports: [],
+  declarations: [],
+  providers: [ApiService],
+})
+export class BusinessModule { }
