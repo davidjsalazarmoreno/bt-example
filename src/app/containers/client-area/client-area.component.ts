a1e2c15a3153b8f0365a7359fc997f53d216c229
David Jose Salazar Moreno <dsalazmo@usersad.everis.int>
2018-07-09 17:40:26 -0500

    refactor(client-area.component.ts): Move logic from AppComponent to client area container.

--- src/app/containers/client-area/client-area.component.ts
+++ src/app/containers/client-area/client-area.component.ts
@@ -1,15 +1,108 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,  } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+
+import { IExchange } from '../../components/exchanges-selector/exchanges-selector.component';
+import { isLessThanTenMinutes } from '../../commons/index';
+
+const controlsConfig = {
+  availableExchanges: {
+    name: 'availableExchanges',
+  },
+  from: {
+    name: 'from',
+  },
+  to: {
+    name: 'to',
+  }
+};
+
+export interface IPreviousConversions {
+  [key: string]: {
+    result: number,
+    createdAt: Date,
+  };
+}
 
 @Component({
-  selector: 'app-client-area',
-  templateUrl: './client-area.component.html',
-  styleUrls: ['./client-area.component.sass']
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
 })
-export class ClientAreaComponent implements OnInit {
+export class AppComponent implements OnInit {
+  public availableExchanges: Array<IExchange> = [];
+  public controlsConfig = controlsConfig;
+  public form: FormGroup;
 
-  constructor() { }
+  fromOptions = {};
+  toOptions = {};
 
-  ngOnInit() {
+  constructor(
+    private _httpClient: HttpClient,
+    private _formBuilder: FormBuilder,
+  ) {}
+
+  public ngOnInit() {
+    this.buildForm();
+
+    this._httpClient.get<Array<IExchange>>('http://localhost:7777/api/v1/parameters', {
+      params: {
+        parameter: 'exchanges'
+      }
+    }).subscribe((response) => {
+      this.availableExchanges = response;
+    }, (error) => {
+      throw new Error(error);
+    });
   }
+
+  public buildForm() {
+    this.form = this._formBuilder.group({
+      [controlsConfig.availableExchanges.name]: ['', Validators.required],
+      [controlsConfig.from.name]: ['', Validators.required],
+      [controlsConfig.to.name]: [{value: '', disabled: true}],
+    });
+  }
+
+  onExchangeSelect() {
+    const selectedIndex: number = this.form.controls[controlsConfig.availableExchanges.name].value;
+    const { from, to } = this.availableExchanges[selectedIndex].currencies;
+
+    this.fromOptions = { prefix: from, thousands: ',', decimal: '.' };
+    this.toOptions = { prefix: to, thousands: ',', decimal: '.' };
+
+  }
+
+  onSubmit(): void {
+    const selectedIndex: number = this.form.controls[controlsConfig.availableExchanges.name].value;
+    const { from, to } = this.availableExchanges[selectedIndex].currencies;
+    const fromAmount: number = this.form.controls[controlsConfig.from.name].value;
+    const currentConversionKey = `${from}-${to}-${fromAmount}`;
+    const previousConversions: IPreviousConversions = JSON.parse(window.sessionStorage.getItem('previousConversions')) || {};
+
+    if (previousConversions.hasOwnProperty(currentConversionKey) && isLessThanTenMinutes(previousConversions[currentConversionKey])) {
+      this.form.controls[controlsConfig.to.name].patchValue(previousConversions[`${from}-${to}-${fromAmount}`].result);
+    } else {
+      this._httpClient.post<{amount: number}>(`http://localhost:7777/api/v1/currency/exchange/${from}/`, {
+        to,
+        amount: fromAmount
+      }).subscribe((result) => {
+        const newResult: IPreviousConversions = {
+          ...previousConversions,
+          [`${from}-${to}-${fromAmount}`]: {
+            result: result.amount,
+            createdAt: new Date(),
+          }
+        };
+
+        this.form.controls[controlsConfig.to.name].patchValue(result.amount);
+        window.sessionStorage.setItem('previousConversions', JSON.stringify(newResult));
+
+      }, (error) => {
+        throw new Error(error);
+      });
+    }
+  }
+
 
 }
