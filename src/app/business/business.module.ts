0307f8daf39d37f54825df2fa9b5e805a54d35e3
David Salazar Moreno <davidjsalazarmoreno@gmail.com>
2018-07-10 00:54:40 -0500

    refactor(bussiness.module.ts): Add an api service.

--- src/app/business/business.module.ts
+++ src/app/business/business.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { ApiService } from './services/api.services';
 
 @NgModule({
-  imports: [],
+  imports: [
+    HttpClientModule
+  ],
   exports: [],
   declarations: [],
-  providers: [ApiService],
+  providers: [
+    ApiService
+  ],
 })
 export class BusinessModule { }
--- src/app/business/services/api.services.ts
+++ src/app/business/services/api.services.ts
@@ -1,7 +1,33 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { environment } from 'src/environments/environment';
+import { IExchange } from 'src/app/components/exchanges-selector/exchanges-selector.component';
+
+export type TParameter = 'exchanges';
 
 @Injectable()
 export class ApiService {
 
-  constructor() { }
+  constructor(
+    private _httpClient: HttpClient,
+  ) { }
+
+  getParameters(parameter: TParameter): Observable<Array<IExchange>> {
+    return this._httpClient.get<Array<IExchange>>(`${environment.apiEndpoint}/api/v1/parameters`, {
+      params: {
+        parameter
+      }
+    });
+  }
+
+  postCurrenciesExchanges(body: {
+    from: string,
+    to: string,
+    amount: number,
+  }): Observable<{amount: number}> {
+    const { from } = body;
+    return this._httpClient.post<{amount: number}>(`${environment.apiEndpoint}/api/v1/currencies/exchanges/${from}/`, body);
+  }
 }
