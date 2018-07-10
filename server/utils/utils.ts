7071c527fccd4230d0416fbb5a5818084c3b96ad
David Salazar Moreno <davidjsalazarmoreno@gmail.com>
2018-07-10 00:52:10 -0500

    chore(util.ts): Format file.

--- server/utils/utils.ts
+++ server/utils/utils.ts
@@ -2,6 +2,7 @@ export function errorHandler(err, _, res, next) {
   if (res.headersSent) {
     return next(err);
   }
+
   res.status(500).json({
     errorType: 'ServerError',
     message: 'Internal server error',
